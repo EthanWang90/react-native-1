e006fc02f1a486f93ce96a9c53865d6caf3cdf42
Ethan Wang <ethanwang9011@gmail.com>
2018-08-07 16:35:35 +1200

    react native 'Hello World'

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+//import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text} from 'react-native';
+
+//type Props = {};
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>HelloWorld</Text>
+    );
+  }
+}
+
