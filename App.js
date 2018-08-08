b0009802dd62711c58c409346acd31845db7b030
Ethan Wang <ethanwang9011@gmail.com>
2018-08-08 21:35:54 +1200

    Test the props transmission in react native

--- App.js
+++ App.js
@@ -8,13 +8,30 @@
 
 import React, {Component} from 'react';
 //import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
+
+class Greeting extends Component{
+  // constructor(props){
+  //   super(props);
+  // }
+  render(){
+    return(
+      <Text>{this.props.name}</Text>
+    );
+  }
+}
 
 //type Props = {};
 export default class HelloWorldApp extends Component {
   render() {
-    return (
-      <Text>HelloWorld</Text>
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return ( // There is no div, only View in react-native
+      <View>
+        <Image source={pic} style={{width: 193, height: 110}} />
+        <Greeting name="Ethan Wang" />
+      </View>
     );
   }
 }
